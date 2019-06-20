3320396fe5b2e718b4d977b67551ba73f77fe4ce
Mario Lopez <marioolopez21@gmail.com>
2019-06-20 17:15:29 -0500

    correcciones de diseño
    Se corrige el modelo de clases, el diagrama de entidad relación y el script de la base de datos

--- Diseño/prodep.sql
+++ Diseño/prodep.sql
@@ -1,6 +1,7 @@
+drop database if exists prodep;
 CREATE DATABASE IF NOT EXISTS prodep;
 use prodep;
- 
+
 create table if not exists docente(
 doc_id int not null auto_increment,
 doc_numeroPersonal int,
@@ -27,6 +28,120 @@ create table if not exists personal(
 per_id int not null auto_increment,
 per_nombre varchar(50),
 per_correo varchar(50),
-
+per_rol varchar(50),
+primary key(per_id)
 )ENGINE=INNODB;
 
+create table if not exists usuario(
+usu_id int primary key not null auto_increment,
+usu_nombre varchar(70),
+usu_contrasena varchar(70),
+per_id int,
+foreign key(per_id) references personal (per_id)
+)ENGINE=INNODB;
+
+create table if not exists areaEducativa(
+are_id int not null auto_increment,
+are_nombre varchar(50),
+primary key(are_id)
+)ENGINE=INNODB;
+
+create table if not exists entidadEducativa(
+ent_id int not null auto_increment,
+ent_nombre varchar(50),
+primary key(ent_id)
+)ENGINE=INNODB;
+
+create table if not exists region(
+reg_id int not null,
+reg_nombre varchar(50),
+primary key(reg_id)
+)ENGINE=INNODB;
+
+create  table if not exists convocatoria(
+con_id int not null auto_increment,
+con_nombre varchar(70),
+primary key(con_id)
+)ENGINE=INNODB;
+
+create table if not exists destinatario(
+des_id int not null auto_increment,
+des_nombre varchar(50),
+primary key(des_id)
+)ENGINE=INNODB;
+
+create table if not exists proyecto(
+pro_id int not null auto_increment,
+pro_claveProgramtica varchar(50),
+pro_folioProdep varchar(50),
+pro_oficioAutorizacion varchar(50),
+pro_inicioApoyo date,
+pro_estado bool,
+pro_numeroDependencia int,
+doc_id int,
+foreign key(doc_id) references docente(doc_id),
+adm_id int,
+foreign key(adm_id) references administrador(adm_id),
+dir_id int,
+foreign key(dir_id) references director(dir_id),
+ent_id int,
+foreign key(ent_id) references entidadEducativa(ent_id),
+per_id int,
+foreign key(per_id) references personal(per_id),
+are_id int,
+foreign key(are_id) references areaEducativa (are_id),
+reg_id int,
+foreign key(reg_id) references region(reg_id),
+con_id int,
+foreign key(con_id) references convocatoria(con_id),
+primary key(pro_id)
+)ENGINE=INNODB;
+
+create table if not exists oficio(
+ofi_id int not null auto_increment,
+ofi_numeroOficio int,
+ofi_anoConvocatoria date,
+ofi_folioProdep varchar(70),
+ofi_asunto text,
+ofi_monto varchar(40),
+ofi_fechaEnvio date,
+ofi_fechaRespuesta date,
+ofi_aprobado bool,
+des_id int,
+foreign key(des_id) references destinatario(des_id),
+doc_id int,
+foreign key(doc_id) references docente(doc_id),
+con_id int,
+foreign key (con_id) references convocatoria(con_id),
+pro_id int,
+foreign key(pro_id) references proyecto(pro_id),
+primary key(ofi_id)
+)ENGINE=INNODB;
+
+create table if not exists becario(
+bec_id int not null auto_increment,
+bec_nombre varchar(70),
+bec_fechaInscripcion varchar(70),
+primary key(bec_id)
+)ENGINE=INNODB;
+
+create table if not exists registroConvocatoria(
+reg_id int not null auto_increment,
+reg_anoConvocatoria date,
+reg_estado bool,
+reg_fechaVoBo date,
+pro_id int,
+foreign key(pro_id) references proyecto(pro_id),
+bec_id int,
+foreign key(bec_id) references becario(bec_id),
+primary key(reg_id)
+)ENGINE=INNODB;
+
+create table if not exists documento(
+doc_id int not null auto_increment,
+doc_nombre varchar(70),
+doc_estado bool,
+reg_id int,
+foreign key(reg_id) references registroConvocatoria(reg_id),
+primary key(doc_id)
+)ENGINE=INNODB;

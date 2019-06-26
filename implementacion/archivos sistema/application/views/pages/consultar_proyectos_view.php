d75b09b694d0d2ca271544469bd0ff8d95c890ca
Mario Lopez <marioolopez21@gmail.com>
2019-06-26 17:01:51 -0500

    Implementación de la interfaz gráfica de consulta dep royectos terminada
    Se implementa la interfaz gráfica de consulta de proyectos

--- implementacion/archivos sistema/application/views/pages/consultar_proyectos_view.php
+++ implementacion/archivos sistema/application/views/pages/consultar_proyectos_view.php
@@ -1,39 +1,102 @@
-<style type="text/css">
-		.scroll{
-			overflow: auto;
-			position: absolute;
-			width: 50%;
-			height: 50%;
-		}
-		.contenido{
-			background-color: gray;
-		}
-		.center{
-			text-align: center;
-		}
-		.margen{
-			margin-top: 3em;
-		}
-	</style>
+ <link rel="stylesheet" href="<?=base_url('css/consultarProyectos_style.css');?>" type="text/css">
 	<div class="contenido center">
-		<div>
-			<form class="margen">
+		<div class="margin-sup">
+			<form>
 				<span>Buscar proyecto:</span>
-				<input type="input" name="" placeholder="Buscar...">
-				<input type="submit" name="" value="Buscar">
+				<input type="input" name="" placeholder="Nombre docente / número proyecto..." class="textbox tamano">
+				<input type="submit" name="buscar" value="Buscar" class="btn btn-success">
 			</form>
 		</div>
-		<div class="scroll margen">
-			<div>
-				<div>
-					<span>Número personal:</span>
-					<span>19346</span>
+		<div class="scrollProyectos">
+
+			<div class="proyectoItem">
+				<div class="imagenProyectoRegistro izquierda">
+					<img src="<?=base_url('recursos/proyectoRegistro.svg')?>">
+				</div>	
+				<div class="datosProyecto">
+					<div>
+						<span>Número de personal:</span>
+						<span>19346</span>
+					</div>
+					<div>
+						<span>Responsable:</span>
+						<span>Luis G. Montané Jimenez</span>
+					</div>
 				</div>
-				<div>
-					<span>Responsable:</span>
-					<span>Luis G. Montané Jimenez</span>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/ver.svg')?>">
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/editar.svg')?>">
 				</div>
 			</div>
+
+			<div class="proyectoItem">
+				<div class="imagenProyectoRegistro izquierda">
+					<img src="<?=base_url('recursos/proyectoRegistro.svg')?>">
+				</div>	
+				<div class="datosProyecto">
+					<div>
+						<span>Número de personal:</span>
+						<span>19346</span>
+					</div>
+					<div>
+						<span>Responsable:</span>
+						<span>Luis G. Montané Jimenez</span>
+					</div>
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/ver.svg')?>">
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/editar.svg')?>">
+				</div>
+			</div>
+
+			<div class="proyectoItem">
+				<div class="imagenProyectoRegistro izquierda">
+					<img src="<?=base_url('recursos/proyectoRegistro.svg')?>">
+				</div>	
+				<div class="datosProyecto">
+					<div>
+						<span>Número de personal:</span>
+						<span>19346</span>
+					</div>
+					<div>
+						<span>Responsable:</span>
+						<span>Luis G. Montané Jimenez</span>
+					</div>
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/ver.svg')?>">
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/editar.svg')?>">
+				</div>
+			</div>
+
+			<div class="proyectoItem">
+				<div class="imagenProyectoRegistro izquierda">
+					<img src="<?=base_url('recursos/proyectoRegistro.svg')?>">
+				</div>	
+				<div class="datosProyecto">
+					<div>
+						<span>Número de personal:</span>
+						<span>19346</span>
+					</div>
+					<div>
+						<span>Responsable:</span>
+						<span>Luis G. Montané Jimenez</span>
+					</div>
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/ver.svg')?>">
+				</div>
+				<div class="imagenProyectoRegistroVista logo">
+					<img src="<?=base_url('recursos/editar.svg')?>">
+				</div>
+			</div>
+
 		</div>
 	</div>
 </body>
